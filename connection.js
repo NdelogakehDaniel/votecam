cc38a93a09c6ba0a79bf551cc6b0a1e7350c4ae5
NdelogakehDaniel <ndabosed@gmail.com>
2022-05-23 23:01:12 +0100

    Modified Connection page

--- connection.js
+++ connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql')
 
-const dbConn = mysql.createConnection({
-    host:'localhost',
-    database:'voteapp',
-    user:'root',
-    password:''
-})
+// const dbConn = mysql.createConnection({
+//     host:'localhost',
+//     database:'voteapp',
+//     user:'root',
+//     password:''
+// })
+
+const dbConn = mysql.createConnection("mysql://b3f73dee5a53e7:d146d8d9@cleardb-tetrahedral-95578/heroku_1233b7754a473f4?reconnect=true")
 
 module.exports = dbConn;
