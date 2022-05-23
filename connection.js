61329f57bcd19ff2b7de9fd7fcc0246fd843faef
NdelogakehDaniel <ndabosed@gmail.com>
2022-05-23 23:37:14 +0100

    Removed s from the connection.js file

--- connection.js
+++ connection.js
@@ -8,5 +8,4 @@ const mysql = require('mysql')
 // })
 
 const dbConn = mysql.createConnection("mysql://b3f73dee5a53e7:d146d8d9@us-cdbr-east-05.cleardb.net/heroku_1233b7754a473f4?reconnect=true")
-s
 module.exports = dbConn;
